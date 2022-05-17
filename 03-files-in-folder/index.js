5fa4fdc4764492729031379ab25de99941777aef
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-05-17 12:58:46 +0600

    feat: solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const path =require('path');
+const fs = require('fs');
+const { stdout } = process;
+
+const targetDir = path.join(__dirname, 'secret-folder');
+
+const showStatisticDir = (pathToDir) => {
+  fs.readdir(pathToDir, (err, files) => {
+    files.forEach(file => showStatisticDirHelper(pathToDir, file));
+  });
+};
+
+const showStatisticDirHelper = (pathToDir, file) => {
+  let currentPath = path.join(pathToDir, file);
+  fs.stat(currentPath, (err, stats) => {
+    if (stats.isFile()) {
+      stdout.write(showStatisticFile(currentPath, stats));
+    } else {
+      showStatisticDir(path.join(pathToDir, file));
+    }
+  });
+};
+
+const showStatisticFile = (pathToFile, stats) => {
+  let ext = path.extname(pathToFile) ?  ' - ' + path.extname(pathToFile) : '';
+  let size = stats.size / 1000 + 'kb';
+  return path.parse(pathToFile).name + ext + ' - ' + size + '\n';
+};
+
+showStatisticDir(targetDir);
