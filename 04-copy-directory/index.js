645fbbc70f53e485fe559b45e3315d837d2d89d2
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-05-20 12:26:35 +0600

    fix: remove waste characters from 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,7 +6,7 @@ const copyDirectory = path.join(__dirname, 'files-copy');
 
 async function copy(targetDirectory, copyDirectory) {
   await fs.promises.rm(copyDirectory, { recursive: true, force: true });
-  await fs.promises.mkdir(copyDirectory, { recursive: true }пш);
+  await fs.promises.mkdir(copyDirectory, { recursive: true });
   fs.readdir(targetDirectory, (err, files) => {
     files.forEach(file => {
       fs.copyFile(path.join(targetDirectory, file), path.join(copyDirectory, file), errCallback);
