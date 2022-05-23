bdfddd97859c27cec5281eda3a1bc1524be5d767
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-05-23 11:04:30 +0600

    fix: 04-copy-dir split on module

---
+++ 04-copy-directory/copymodule/copy.js
@@ -0,0 +1,18 @@
+module.exports = copy;
+
+const path = require('path');
+const fs = require('fs');
+
+async function copy(targetDirectory, copyDirectory) {
+  await fs.promises.rm(copyDirectory, { recursive: true, force: true });
+  await fs.promises.mkdir(copyDirectory, { recursive: true });
+  fs.readdir(targetDirectory, (err, files) => {
+    files.forEach(file => {
+      fs.copyFile(path.join(targetDirectory, file), path.join(copyDirectory, file), errCallback);
+    });
+  });
+}
+
+function errCallback(err) {
+  if (err) throw err;
+}
